16705fcd5ab798dbe39527cf80e043a601cece70
Franci Sheshi <franci.sheshi2017@gmail.com>
2023-04-01 15:09:13 +0200

    Refactoring page1 and adding button, fixing style of elements in each page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menus from './views/menu';
-import Title1 from './views/pages/title-1';
+import ButtonComponent from './views/pages/Button';
 import Title2 from './views/pages/title-2';
 
 import './App.css';
@@ -14,7 +14,7 @@ const App = () => {
         <Menus />
         
         <Routes>
-          <Route path='/pages/page-1' element={<Title1 />} />
+          <Route path='/pages/button' element={<ButtonComponent />} />
           <Route path='/pages/page-2' element={<Title2 />} />
         </Routes>
       </Router>
